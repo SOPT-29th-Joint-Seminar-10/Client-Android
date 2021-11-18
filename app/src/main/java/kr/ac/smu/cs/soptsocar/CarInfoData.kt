fb1e31bcd2a58ea3304b4c7e1098ef49b68df45f
kimsubeen <papajj06@naver.com>
2021-11-18 23:59:27 +0900

    [ADD] 차량예약 recyclerview, adapter
    + build.gradle  glide 이미지  implementation

---
+++ app/src/main/java/kr/ac/smu/cs/soptsocar/CarInfoData.kt
@@ -0,0 +1,11 @@
+package kr.ac.smu.cs.soptsocar
+
+data class CarInfoData(
+    val car : Int,
+    val heart: Int,
+    val name : String,
+    val price : String,
+    val sale : String,
+    val date : String,
+    val city : String,
+)
--- app/src/main/java/kr/ac/smu/cs/soptsocar/ReservationCar.kt
+++ app/src/main/java/kr/ac/smu/cs/soptsocar/ReservationCar.kt
@@ -2,10 +2,50 @@ package kr.ac.smu.cs.soptsocar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.ac.smu.cs.soptsocar.databinding.ActivityReservationCarBinding
 
 class ReservationCar : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReservationCarBinding
+    lateinit var reserveCarAdapter: ReserveCarAdapter
+
+    val data = mutableListOf<CarInfoData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservation_car)
+        binding= ActivityReservationCarBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        initAdapter()
+
     }
+
+
+
+private fun initAdapter(){
+
+    reserveCarAdapter= ReserveCarAdapter()
+    binding.rvCar.adapter=reserveCarAdapter
+    val img1 = R.drawable.img_tosan
+    val img11 = R.drawable.ic_default_heart_ios
+
+
+    reserveCarAdapter.carList.addAll(
+        listOf(
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천"),
+            CarInfoData(img1,img11,"투싼(경유)","월 503,000원~","26%","2016~2017", "서울/경기/인천")
+        )
+    )
+    reserveCarAdapter.notifyDataSetChanged()
+}
+
 }
